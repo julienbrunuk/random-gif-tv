96efc158390a6b757a083780c5a058f7529cd163
Bart Tomzynski <barttomzynski@builtvisible.com>
2015-03-03 11:49:04 +0000

    first commit full of awesomness

---
+++ scripts/tv.js
@@ -0,0 +1,35 @@
+(function () {
+	var endpoint = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC';
+	var loaded = false;
+
+	function randomGif() {
+		var jqxhr = $.ajax(endpoint)
+			.done(function (response) {
+				console.log(response);
+
+				var img = response.data.image_original_url;
+
+				$('#tv').css({
+					'background-image': 'url(' + img + ')'
+				});
+
+				setTimeout(randomGif, 30 * 1000);
+			})
+			.fail(function () {
+				console.log('error');
+			})
+			.always(function () {
+				console.log('complete');
+			});
+
+			// Perform other work here ...
+
+			// Set another completion function for the request above
+			jqxhr.always(function () {
+				console.log('second complete');
+			});
+	}
+
+	randomGif();
+
+})();
